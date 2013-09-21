56d3e170038497a14fd9ea289de2d223218894d9
Anand Srinivasan <anandtech1532@gmail.com>
2013-09-21 14:08:51 -0400

    0.0

--- infonet_example.py
+++ infonet_example.py
@@ -36,32 +36,7 @@ load=['testX','testY','pX','pY','pXX','pYY','w','f','adj']
 loaded=repo.loadData('infonet_vars.db',nums,lendata,numClasses,load)
 X,Y,pX,pY,pXX,pYY,w,f,adj=loaded
 
-
-
-size=int(sqrt(len(pX)))
-nb=repo.allNeighbors(len(pX))
-propscores=zeros(len(Y))
-
-for img in range(len(Y)):
-    print 'image being processed: '+repr(img)
-    propsc=[None for _ in range(len(pX))]
-
-    fromNode=random.randint(0,len(pX)) #start somewhere random
-    prevNode=nb[fromNode][random.randint(0,len(nb[fromNode]))]
-    
-    propsc[prevNode]=repo.localInd(prevNode,pX,pY,Y[img],w)
-    propsc[fromNode]=repo.localFunc(pX,pY,pXX,pYY,w,fromNode,prevNode,\
-                                    propsc[prevNode],Y[img])
-                                    
-    while(propsc.count(None)>5):
-        temp=random.randint(0,len(nb[fromNode]))
-        thisNode=nb[fromNode][temp] 
-        if (thisNode==prevNode):thisNode=nb[fromNode][(temp+1)%len(nb[fromNode])]
-        propsc[thisNode]=repo.localFunc(pX,pY,pXX,pYY,w,thisNode,
-            fromNode,propsc[fromNode],Y[img])
-        prevNode=fromNode
-        fromNode=thisNode
-    propscores[img]=sum([e for e in propsc if e is not None])
+propscores=pgm.propscore(loaded,600)
         
 print 'propscored'
 sc=[repo.indScore(pX,pY,Y,w,res),repo.corScore(Y,f,adj),
@@ -69,4 +44,4 @@ sc=[repo.indScore(pX,pY,Y,w,res),repo.corScore(Y,f,adj),
     
 print 'scored imgs'
 
-repo.showplot(sc,nums,showpics=True,X=X,Y=Y)
+repo.showplot(sc,nums,X=X,Y=Y)
--- pgm.py
+++ pgm.py
@@ -8,4 +8,36 @@ from scipy.linalg import *
 from scipy import ndimage
 from scipy import stats
 
+import infonet_fin as repo
+
+##propscoring #################
+def propscore(loaded,iters):
+    X,Y,pX,pY,pXX,pYY,w,f,adj=loaded
+    size=int(sqrt(len(pX)))
+    nb=repo.allNeighbors(len(pX))
+    propscores=zeros(len(Y))
+
+    for img in range(len(Y)):
+        print 'image being processed: '+repr(img)
+        propsc=[None for _ in range(len(pX))]
+
+        fromNode=random.randint(0,len(pX)) #start somewhere random
+        prevNode=nb[fromNode][random.randint(0,len(nb[fromNode]))]
+        
+        propsc[prevNode]=repo.localInd(prevNode,pX,pY,Y[img],w)
+        propsc[fromNode]=repo.localFunc(pX,pY,pXX,pYY,w,fromNode,prevNode,\
+                                        propsc[prevNode],Y[img])
+        ind=0              
+        while(ind<iters):
+            temp=random.randint(0,len(nb[fromNode]))
+            thisNode=nb[fromNode][temp] 
+            if (thisNode==prevNode):thisNode=nb[fromNode][(temp+1)%len(nb[fromNode])]
+            propsc[thisNode]=repo.localFunc(pX,pY,pXX,pYY,w,thisNode,
+                fromNode,propsc[fromNode],Y[img])
+            prevNode=fromNode
+            fromNode=thisNode
+            ind+=1
+        propscores[img]=sum([e for e in propsc if e is not None])
+    return propscores
+
 ############################  template stuff ###########################
